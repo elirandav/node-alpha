76bb513c36ea77aca52e585fdfbe13c97df7a232
eliran.david <eliran.david@nielsen.com>
2019-12-05 16:06:54 +0200

    npm uninstall constantinople due to security alert

--- services/services.js
+++ services/services.js
@@ -7,6 +7,6 @@ module.exports.getUsers = async (req, res) => {
     const user1 = 'Tim';
     const user2 = 'Tam';
 
-    const message = getUsers(user1, user2);
+    const message = sumUsers(user1, user2);
     return res.status(200).send(message)
 };
