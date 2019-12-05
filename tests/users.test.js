a7cac17991b6c824c552ac6af8d75a0a2a600678
eliran.david <eliran.david@nielsen.com>
2019-12-05 18:53:34 +0200

    add require('../routes')(app)

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,12 +1,14 @@
 const test = require('ava');
-
 const request = require('supertest');
 const bodyParser = require('body-parser');
 const express = require('express');
+const app = express()
+    .use(bodyParser.json());
 
+require('../routes')(app);
 
-test('users', () => {
-    request(app)
+test('users',async () => {
+    await request(app)
         .get('/users')
         .expect(200);
 });
